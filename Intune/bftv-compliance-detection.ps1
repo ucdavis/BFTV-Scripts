bb6637e6ce5171d3ce3b6d878b1cb95fa3ee6494
jjv-ucd <jjvidaca@ucdavis.edu>
2025-02-18 16:24:43 -0800

    Added Trellix detection.

--- Intune/bftv-compliance-detection.ps1
+++ Intune/bftv-compliance-detection.ps1
@@ -6,8 +6,10 @@ if ($BIOS_test)
 	$BIOS_PW = $true
 }
 
-# Check if BigFix is Installed
+# Collect installed software
 $InstalledSoftware = Get-ItemProperty HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*
+
+# Check if BigFix is Installed
 if ($InstalledSoftware.DisplayName -like "BigFix Client") {
 	$DetectBigFix = "Detected"
 }
@@ -15,6 +17,14 @@ else {
 	$DetectBigFix = "Not Detected"
 }
 
+# Check if Trellix is Installed
+if ($InstalledSoftware.DisplayName -like "Trellix Endpoint Security*") {
+	$DetectTrellix = "Detected"
+}
+else {
+	$DetectTrellix = "Not Detected"
+}
+
 # Output variables
-$output = @{ BIOS_PW = $BIOS_PW; DetectBigFix = $DetectBigFix}
+$output = @{ BIOS_PW = $BIOS_PW; DetectBigFix = $DetectBigFix; DetectTrellix = $DetectTrellix}
 return $output | ConvertTo-Json -Compress
